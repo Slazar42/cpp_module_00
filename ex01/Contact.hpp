2c47c7fcd6554d0e4688aff45fd4eafa9d3b2643
Salah eddine Lazar <slazar@c1r4p9.1337.ma>
2023-10-15 13:59:59 +0100

    first

---
+++ ex01/Contact.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slazar <slazar@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 11:35:15 by slazar            #+#    #+#             */
+/*   Updated: 2023/10/15 13:08:54 by slazar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PhoneBook.hpp"
+
+class Contact
+{
+    private :
+        std::string f_name;
+        std::string l_name;
+        std::string n_name;
+        std::string d_s;
+        std::string d_s;
+        
+};
