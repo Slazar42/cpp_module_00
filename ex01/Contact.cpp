8d1759d7c26e07313e604fc5efc05156f033cd2a
Salah eddine Lazar <slazar@c1r4p9.1337.ma>
2023-10-17 10:20:29 +0100

    to be continued

---
+++ ex01/Contact.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slazar <slazar@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 10:00:59 by slazar            #+#    #+#             */
+/*   Updated: 2023/10/17 10:18:34 by slazar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contact.hpp"
+
+void::Contact set_f_n(std::string f)
+{
+    f_name = f;
+}
+void::Contact set_l_n(std::string l)
+{
+    l_name = l;
+}
+void::Contact set_n_n(std::string n)
+{
+    n_name = n;
+}
+void::Contact set_f_n(std::string ds)
+{
+    return d_s;
+}
+string::Contact set_f_n(std::string f)
+{
+    return f_name;
+}
+string::Contact set_l_n(std::string l)
+{
+    return l_name;
+}
+string::Contact set_n_n(std::string n)
+{
+    return n_name;
+}
+string::Contact set_ds(std::string ds)
+{
+    return d_s;
+}
--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: slazar <slazar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 11:35:15 by slazar            #+#    #+#             */
-/*   Updated: 2023/10/15 13:08:54 by slazar           ###   ########.fr       */
+/*   Updated: 2023/10/17 10:18:20 by slazar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,13 @@ class Contact
         std::string n_name;
         std::string d_s;
         std::string d_s;
-        
+    public :
+    void set_f_n(std::string f);
+    void set_l_n(std::string l);
+    void set_n_n(std::string n);
+    void set_ds(std::string ds);
+    std::string get_f_n();
+    std::string get_l_n();
+    std::string get_n_n();
+    std::string get_ds();
 };
